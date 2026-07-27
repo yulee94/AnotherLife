0ed6bcd8385256ddc2abe5326d11292c3a421705
yulee94 <yulee941226@gmail.com>
2026-07-27 12:58:40 +0900

    Clean up Android theme edge-to-edge

--- app/src/main/java/com/example/anotherlife/MainActivity.kt
+++ app/src/main/java/com/example/anotherlife/MainActivity.kt
@@ -9,8 +9,8 @@ import com.example.anotherlife.ui.theme.AnotherLifeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContent {
             AnotherLifeTheme {
                 AnotherLifeShell()
